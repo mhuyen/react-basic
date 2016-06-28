a72581b9f0b8d1c06907468b849cacbe80eaf0fa
Manvi Huyen <manvi.huyen@gmail.com>
2016-06-28 16:16:18 -0700

    Full

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Components = require('./components/components.jsx');
+
+ReactDOM.render (
+  <Components />,
+  document.getElementById('app')
+);
